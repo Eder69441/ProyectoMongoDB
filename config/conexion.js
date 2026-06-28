12cd45fb0333ad862021eebd7f0e2feb1d3c6b00
Ederlin Ortega <ederling91@gmail.com>
2026-06-28 15:45:09 -0400

    Corrección de seguridad

--- config/conexion.js
+++ config/conexion.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // Conexión con MongoDB
-mongoose.connect("mongodb://ederling91:8494997343@mongo.ia3x.com:27017/ederling91_proyectofinal?authSource=admin")
+mongoose.connect(process.env.MONGO_DB_URI)
 .then(() => {
     console.log("Conectado a MongoDB");
 })
