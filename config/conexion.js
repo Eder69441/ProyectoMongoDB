6b1d212bc56be05e7a3ab4888a73e406b00e3cd0
Ederlin Ortega <ederling91@gmail.com>
2026-06-28 15:28:01 -0400

    Modify MongoDB connection string
    Updated MongoDB connection string to remove database name.

--- config/conexion.js
+++ config/conexion.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // Conexión con MongoDB
-mongoose.connect("mongodb://ederling91:8494997343@mongo.ia3x.com:27017/ederling91_proyectofinal?authSource=admin")
+mongoose.connect("mongodb://ederling91:8494997343@mongo.ia3x.com:27017/?authSource=")
 .then(() => {
     console.log("Conectado a MongoDB");
 })
