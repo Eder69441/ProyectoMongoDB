f4aac8ca94d003fbd490d59ebb037534bd2f7b72
Ederlin Ortega <ederling91@gmail.com>
2026-06-28 15:28:25 -0400

    Update MongoDB connection string for security
    Removed username and password from MongoDB connection string.

--- config/conexion.js
+++ config/conexion.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 // Conexión con MongoDB
-mongoose.connect("mongodb://ederling91:8494997343@mongo.ia3x.com:27017/?authSource=")
+mongoose.connect("mongodb://@mongo.ia3x.com:27017/?authSource=")
 .then(() => {
     console.log("Conectado a MongoDB");
 })
